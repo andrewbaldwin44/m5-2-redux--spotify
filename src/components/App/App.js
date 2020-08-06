a94e4e21b56f518ec70acfb2e267282669e2701f
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-06 11:51:25 -0400

    Solution to exercise 6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import GlobalStyles from "../GlobalStyles";
+
+import {
+  requestAccessToken,
+  receiveAccessToken,
+  receiveAccessTokenError
+} from "../../action";
 
 import ArtistRoute from "../ArtistRoute";
 
 const DEFAULT_ARTIST_ID = '6XyY86QOPPrYVGvF9ch6wz';
 
 const App = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(requestAccessToken());
+
+    fetch("/spotify_access_token")
+      .then(response => response.json())
+      .then(data => dispatch(receiveAccessToken(data.access_token)))
+      .catch((error) => dispatch(receiveAccessTokenError()));
+  }, []);
+
   return (
     <Router>
       <GlobalStyles />
--- src/components/ArtistRoute.js
+++ src/components/ArtistRoute.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 
 function ArtistRoute() {
+  const accessToken = useSelector((state) => state.auth.token);
+
   return (
-    <div>Artist</div>
+    <div>{accessToken}</div>
   )
 }
 
